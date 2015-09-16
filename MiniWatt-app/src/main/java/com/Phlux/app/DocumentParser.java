0e3b9162913b21f17564cb4b93b5c413fe767a1b
Craig Wilhite <cwilhit@purdue.edu>
2015-09-16 14:47:02 -0400

    Doc Parser
    Adding document parser class. This class will parse pdf documents into
    strings.

---
+++ MiniWatt-app/src/main/java/com/Phlux/app/DocumentParser.java
@@ -0,0 +1,13 @@
+package com.Phlux.app;
+
+/**
+ * Class used by back-end to parse PDF documents
+ *
+ */
+public class DocumentParser 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
