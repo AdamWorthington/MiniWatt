38f6f08cf7e32d0a475b9b65ca5da3c612c166a9
gjochums <gjochums@purdue.edu>
2015-09-20 14:04:25 -0400

    Added the Question Object and it's data structures and getter/setter methods. Started working on question parser

---
+++ MiniWatt-app/src/main/java/com/Phlux/app/Question.java
@@ -0,0 +1,107 @@
+package com.Phlux.app;
+
+import java.util.*;
+
+/*
+ * text of the question
+ */
+private String question_text;
+/*
+ * The kind of question enumerated
+ * 1 = what
+ * 2 = when
+ * 3 = where
+ * 4 = who
+ * 5 = how
+ * 6 = why
+ * 7 = which
+ */
+private int type;
+/*
+ * The subject of the question
+ */
+String subject_text;
+/*
+ * The prepositions linked to the subject
+ */
+LinkedList<String> subject_prepositions;
+/*
+ * The predicate of the question
+ */
+String predicate_text;
+/*
+ * The prepositions linked to the predicate
+ */
+LinkedList<String> predicate_prepositions;
+/*
+ * Object that holds all of the information in a question
+ */
+public class Question
+{
+	public Question(String question)
+	{
+		this.question = question_text;
+	}
+
+	/*
+	 * Getter Methods
+	 * ========================================================================
+	 */
+	public int getType()
+	{
+		return type;
+	}
+
+	public Stirng getQuestionText()
+	{
+		return question_text;
+	}
+	
+	public String getSubject()
+	{
+		return subject_text;
+	}
+	
+	public String getPredicate()
+	{
+		return predicate_text;
+	}
+	
+	public LinkedList<String> getSubjectPrepositions()
+	{
+		return subject_prepositions;
+	}
+	
+	public LinkedList<String> getPredicatePrepositions()
+	{
+		return predicate_prepositions;
+	}
+	/*
+	 * Setter Methods
+	 * ========================================================================
+	 */
+	public void setType(int t)
+	{
+		type = t;
+	}
+	
+	public void setSubject(String subject)
+	{
+		subject_text = subject;
+	}
+	
+	public void setPredicate(String predicate)
+	{
+		predicate_text = predicate;
+	}
+	
+	public void setSubjectPrepositions(LinkedList<String> preps)
+	{
+		subject_prepositions = preps;
+	}
+	
+	public void setPredicatePrepositions(LinkedList<String> preps)
+	{
+		predicate_prepositions = preps;
+	}
+}
---
+++ MiniWatt-app/src/main/java/com/Phlux/app/QuestionParser.java
@@ -0,0 +1,98 @@
+package com.Phlux.app;
+
+import java.util.*;
+
+/*
+ * The class that breaks down a question into a Question object
+ */
+public class QuestionParser
+{
+	
+	public static void parseQuestion(Question question, String questions_text)
+	{
+		int type = getType(question_text);
+		switch(type) {
+		case 1: question.setType = 1;
+				//call method of what questions
+				break;
+		case 2: question.setType = 2;
+				//call method of what questions
+				break;
+		case 3: question.setType = 3;
+				//call method of what questions
+				break;
+		case 4: question.setType = 4;
+				//call method of what questions
+				break;
+		case 5: question.setType = 5;
+				//call method of what questions
+				break;
+		case 6: question.setType = 6;
+				//call method of what questions
+				break;
+		case 7: question.setType = 7;
+				//call method of what questions
+				break;
+		default: question.setType = -1;
+				//error 
+				break;
+		}
+		
+	}
+	
+	public static int getType(String question)
+	{
+		/*
+		 * These may be needed later
+		CharSequence what = "what";
+		CharSequence What = "What";
+		CharSequence when = "when";
+		CharSequence When = "When";
+		CharSequence where = "where";
+		CharSequence Where = "Where";
+		CharSequence who = "who";
+		CharSequence Who = "Who";
+		CharSequence how = "how";
+		CharSequence How = "How";
+		CharSequence why = "why";
+		CharSequence Why = "Why";
+		CharSequence which = "which";
+		CharSequence Which = "Which";*/
+		
+		if(question.contains("What") || question.contains("what"))
+		{
+			return 1;
+		}
+		else if(question.contains("When") || question.contains("when"))
+		{
+			return 2;
+		}
+		else if(question.contains("Where") || question.contains("where"))
+		{
+			return 3;
+		}
+		else if(question.contains("Who") || question.contains("who"))
+		{
+			return 4;
+		}
+		else if(question.contains("How") || question.contains("how"))
+		{
+			return 5;
+		}
+		else if(question.contains("Why") || question.contains("why"))
+		{
+			return 6;
+		}
+		else if(question.contains("Which") || question.contains("which"))
+		{
+			return 7;
+		}
+		else
+		{
+			//no question word found
+			return -1;
+		}
+	}
+	
+	
+}
