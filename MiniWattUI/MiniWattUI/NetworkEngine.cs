26cca60bed2ccc8f6f9596520f555a1ace2ede33
Chris Doak <doak@purdue.edu>
2015-09-22 13:37:52 -0400

    Added the initial MiniWatt User Interface. It doesn't do anything yet.

---
+++ MiniWattUI/MiniWattUI/NetworkEngine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniWattUI
+{
+    enum QuestionType
+    {
+        TEXT,
+        PDF,
+        JPG
+    };
+
+    enum SourceType
+    {
+        TEXT,
+        PDF,
+        JPG,
+        NULL
+    };
+
+    enum ResponseType
+    {
+        TEXT,
+        PDF
+    };
+
+    class NetworkEngine
+    {
+        public void post_question(Object question, QuestionType questionType, Object source, SourceType sourceType, ResponseType responceType)
+        {
+
+        }
+
+        public void recieve_answer()
+        {
+
+        }
+    }
+}
