1712e96400a0f7a8eec600e8e4be9cfea2e78a17
Chris Doak <doak@purdue.edu>
2015-09-24 11:22:43 -0400

    Added a couple of .gitignore files to ignore artifacts. More of the UI has been fleshed out. It's almost done.

--- MiniWattUI/MiniWattUI/MainPage.xaml.cs
+++ MiniWattUI/MiniWattUI/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +24,46 @@ namespace MiniWattUI
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private StorageFile _questionsFile = null;
+        private StorageFile _sourceFile = null;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Menu_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Splitter.IsPaneOpen = !Splitter.IsPaneOpen;
+        }
+
+        private async void QuestionsBrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".jpeg");
+            openPicker.FileTypeFilter.Add(".pdf");
+            _questionsFile = await openPicker.PickSingleFileAsync();
+            if (_questionsFile != null)
+            {
+                QuestionFileChosenText.Visibility = Visibility.Visible;
+                QuestionFileChosenText.Text = "File Chosen: " + _questionsFile.Name;
+                QuestionsClearButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                QuestionFileChosenText.Visibility = Visibility.Collapsed;
+                QuestionFileChosenText.Text = "File Chosen: ";
+            }
+        }
+
+        private void QuestionsClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            _questionsFile = null;
+            QuestionsClearButton.Visibility = Visibility.Collapsed;
+            QuestionFileChosenText.Visibility = Visibility.Collapsed;
         }
     }
 }
