b491c3c345b5c9491adecc4e59f65cd64caa6c5f
Robbie <rmantzoros@gmail.com>
2015-09-24 02:28:30 -0400

    Imported JSONObject and JSONException libraries

--- MiniWattUI/MiniWattUI/NetworkEngine.cs
+++ MiniWattUI/MiniWattUI/NetworkEngine.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Json.JsonObject;
+using System.Json.JsonException;
 
 namespace MiniWattUI
 {
@@ -31,7 +33,17 @@ namespace MiniWattUI
     {
         public void post_question(Object question, QuestionType questionType, Object source, SourceType sourceType, ResponseType responceType)
         {
+            JSONObject quesion = new JSONObject();
 
+            try
+            {
+
+            } catch(JSONException e)
+            {
+                e.printStackTrace();
+            }
+             
+            
         }
 
         public void recieve_answer()
